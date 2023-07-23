e05e2b78f42ac8693c13f092649c145ad5eded50
shub <shubhampahilwani1@gmail.com>
2023-07-23 13:39:39 +0400

    Edit js to use dom manipulation & edit logic

--- javascript.js
+++ javascript.js
@@ -12,26 +12,36 @@ function playRound(ps, cs) {
     if (ps === cs) return `Tie! Both Used ${ps}.`;
     else if (ps === 'Rock' && cs === 'Scissors' || ps === 'Paper' && cs === 'Rock' || ps === 'Scissors' && cs === 'Paper') {
         playerScore++;
+        document.getElementById("ps").textContent++;
         return `You Won! ${ps} Beats ${cs}.`;
     }
     else {
         computerScore++;
+        document.getElementById("cs").textContent++;
         return `You Lost! ${cs} Beats ${ps}.`;
     } 
 }
 
 function game() {
-    for (let i = 0; i < 5; i++){
-        let computerSelection = getComputerChoice();
-        let playerSelection = prompt("Rock/Paper/Scissors: ").toLowerCase();
-        playerSelection = playerSelection.charAt(0).toUpperCase()
-                        + playerSelection.slice(1, playerSelection.length);
-        console.log(playRound(playerSelection, computerSelection));
+    let playerSelection = this.id;
+    let computerSelection = getComputerChoice();
+    document.querySelector('#curr_round').textContent = playRound(playerSelection, computerSelection);
+    if (playerScore == 5 || computerScore == 5){
+        choiceList.forEach(choice => {
+            choice.removeEventListener('click', game);
+        });
+        const result = document.createElement('p');
+        result.textContent = (computerScore > playerScore)? "You Lost!" : "You Won!";
+        document.body.appendChild(result);
     }
 }
 
-game();
+const choiceList = document.querySelectorAll('.choice');
 
+choiceList.forEach(choice => {
+    choice.addEventListener('click', game);
+});
+/*
 if (computerScore == playerScore) alert(`Tie! Player: ${playerScore} | AI: ${computerScore}`);
 else if (computerScore > playerScore) alert(`You Lost! Player: ${playerScore} | AI: ${computerScore}`);
-else alert(`You Won! Player: ${playerScore} | AI: ${computerScore}`);
+else alert(`You Won! Player: ${playerScore} | AI: ${computerScore}`);*/
